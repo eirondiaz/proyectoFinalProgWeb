352479ebdfe8c8df103653948aabb9f0382387f1
Gabriel Geronimo <sr.gabrielgil@gmail.com>
2020-11-22 01:15:00 -0400

    menu 80%

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegistroComponent } from './registro/registro.component';
 
 const routes: Routes = [{path: '', component: LoginComponent},
-                        {path: 'registro', component: RegistroComponent}];
+                        {path: 'registro', component: RegistroComponent},
+                        {path: 'home', component: HomeComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,37 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+ export class HomeComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  OpenMenu(){
+    let menu = document.getElementById('menu-container');
+    let icon = document.getElementById('close-menu-icon')
+
+    if(menu.classList.contains('d-none') || menu.classList.contains('animate__slideOutLeft'))
+        menu.classList.remove('d-none');
+        menu.classList.remove('animate__slideOutLeft');
+        icon.classList.add('d-none');
+        menu.classList.add('animate__slideInLeft');
+    
+  }
+
+  CloseMenu(){
+    let menu = document.getElementById('menu-container');
+    let icon = document.getElementById('close-menu-icon');
+
+    if(!menu.classList.contains('d-none') || menu.classList.contains('animate__slideIntLeft'))
+        menu.classList.remove('animate__slideInLeft');
+        menu.classList.add('animate__slideOutLeft');
+        setTimeout(() => {
+          menu.classList.add('d-none');
+          icon.classList.remove('d-none');
+        }, 430);
+         
+ 
+  }
 }
