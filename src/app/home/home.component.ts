87a46e9765224eecd8f2df31080b1bd6a6a27658
Gabriel Geronimo <sr.gabrielgil@gmail.com>
2020-11-23 20:25:42 -0400

    mejoras ui listado paciente

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,33 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
  export class HomeComponent implements OnInit {
-  isFiltroOpen:boolean = false; 
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  OpenFiltro(){
-    let filtro = document.getElementById('filtro-container');
-
-    if(filtro.classList.contains('d-none') || filtro.classList.contains('animate__slideOutLeft'))
-        filtro.classList.remove('d-none');
-        filtro.classList.remove('animate__slideOutRight');
-        filtro.classList.add('animate__slideInRight');
-        this.isFiltroOpen = true; 
-  }
-
-  CloseFiltro(){
-    let filtro = document.getElementById('filtro-container');
-
-    if(!filtro.classList.contains('d-none') || filtro.classList.contains('animate__slideIntRight'))
-        filtro.classList.remove('animate__slideInRight');
-        filtro.classList.add('animate__slideOutRight');
-        this.isFiltroOpen = false ;
-        setTimeout(() => {
-          filtro.classList.add('d-none');
-        }, 500);
   }
  
 }
