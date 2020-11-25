35ded3e78a73275e5ad300a04ca226fe97ed9f78
Gabriel Geronimo <sr.gabrielgil@gmail.com>
2020-11-25 12:36:07 -0400

    tokes finales a la ui nuevo paciente

--- src/app/nuevo-paciente/nuevo-paciente.component.ts
+++ src/app/nuevo-paciente/nuevo-paciente.component.ts
@@ -7,17 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NuevoPacienteComponent implements OnInit {
   showSelectAlergia:boolean = true; 
+  registroPacienteBoxes:string[] = ['box1', 'box2', 'box3', 'box4'];
+  currentBox = 0; 
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-
-
   CambiarBox(e){
      if(e.target.value == "Otros"){
          this.showSelectAlergia = false; 
      }
+  }
+
+  NextBox(){
+    let activeBox = document.getElementById(this.registroPacienteBoxes[this.currentBox]); 
+    let nextBox = document.getElementById(this.registroPacienteBoxes[this.currentBox + 1]);
+
+    activeBox.classList.add('d-none')
+    nextBox.classList.remove('d-none');
+    this.currentBox += 1;  
+  }
+
+  PreviusBox(){
+    let activeBox = document.getElementById(this.registroPacienteBoxes[this.currentBox]); 
+    let preBox = document.getElementById(this.registroPacienteBoxes[this.currentBox - 1]);
+    
+
+    activeBox.classList.add('d-none')
+    preBox.classList.remove('d-none');
+    this.currentBox -= 1; 
+  }
+
+  saveFoto(e){
+    console.log(e.target.value)
+    
+     this.NextBox(); 
   }
 
 }
