03fa06e62b0a4c04fcd28e57000e2665bf51c74b
oscarmora26 <oscar.rander@outlook.com>
2020-11-25 22:26:51 -0400

    create pacient 1.1

--- src/app/nuevo-paciente/nuevo-paciente.component.ts
+++ src/app/nuevo-paciente/nuevo-paciente.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { Paciente } from '../Models/Paciente';
 import { PacienteService } from './../services/paciente.service'
 
 // Holamundo
@@ -12,12 +13,22 @@ export class NuevoPacienteComponent implements OnInit {
   showSelectAlergia: boolean = true;
   registroPacienteBoxes: string[] = ['box1', 'box2', 'box3', 'box4'];
   currentBox = 0;
-
+  paciente: Paciente
+  @ViewChild('rbt1', {static: true}) rbt1 : ElementRef
+  @ViewChild('rbt2', {static: true}) rbt2 : ElementRef
+  @ViewChild('sangre', {static: true}) sangre : ElementRef
+  @ViewChild('alergia', {static: false}) alergia : ElementRef
+ 
   myForm: FormGroup = this._builder.group({
-    nombre: ['', Validators.compose([Validators.required, Validators.email])],
+    nombre: ['', Validators.required],
     apellido: ['', Validators.required],
-    fechaNac: ['', Validators.required],
-    sexo: ['']
+    fecha_nac: ['', Validators.required],
+    sexo: [''],
+    email: ['', [Validators.required, Validators.email]],
+    cedula: ['', Validators.required],
+    tipo_sangre: ['', Validators.required],
+    alergias: ['', Validators.required],
+    foto: ['']
   })
 
   constructor(
@@ -35,6 +46,12 @@ export class NuevoPacienteComponent implements OnInit {
   }
 
   NextBox() {
+    this.paciente = this.myForm.value
+    this.paciente.sexo = this.getSexo()
+    this.paciente.tipo_sangre = this.getSangre()
+    this.paciente.alergias = this.getAlergias()
+    console.log(this.paciente)    
+
     let activeBox = document.getElementById(this.registroPacienteBoxes[this.currentBox]);
     let nextBox = document.getElementById(this.registroPacienteBoxes[this.currentBox + 1]);
 
@@ -47,7 +64,6 @@ export class NuevoPacienteComponent implements OnInit {
     let activeBox = document.getElementById(this.registroPacienteBoxes[this.currentBox]);
     let preBox = document.getElementById(this.registroPacienteBoxes[this.currentBox - 1]);
 
-
     activeBox.classList.add('d-none')
     preBox.classList.remove('d-none');
     this.currentBox -= 1;
@@ -56,7 +72,50 @@ export class NuevoPacienteComponent implements OnInit {
   saveFoto(e) {
     console.log(e.target.value)
 
+    let fileSelect = e.target.files
+    let file;
+    if(fileSelect.length > 0){
+        file = fileSelect[0]
+        let fileReader = new FileReader()
+
+        fileReader.onload = function (FileLoadevent) {
+            let srcData = FileLoadevent.target.result  
+            localStorage.setItem("imagen", JSON.stringify(srcData))        
+        }
+        fileReader.readAsDataURL(file)
+    }
+
     this.NextBox();
   }
 
+  get nombre () { return this.myForm.get('nombre') }
+  get apellido () { return this.myForm.get('apellido') }
+  get fecha_nac () { return this.myForm.get('fecha_nac') }
+  get email () { return this.myForm.get('email') }
+  get cedula () { return this.myForm.get('cedula') }
+  getSexo(){ return this.rbt1.nativeElement.checked ? "hombre" : "mujer" }
+  getSangre(){ return this.sangre.nativeElement.value }
+  getAlergias(){ 
+    return this.showSelectAlergia ? this.alergia.nativeElement.options[this.alergia.nativeElement.selectedIndex].text : this.alergia.nativeElement.value
+  }
+
+  firstFormValidation(){
+    return this.nombre.valid && this.apellido.valid && this.fecha_nac.valid
+  }
+
+  secondFormValidation(){
+    return this.email.valid && this.cedula.valid
+  }
+
+  createPatient(){
+    this.paciente.foto = localStorage.getItem('imagen')
+    console.log(this.paciente)
+
+    this._pacienteService.createPatient(this.paciente).subscribe(
+      (resp => {
+        console.log("Paciente guardado exitosamente")
+      }),
+      (error => console.log(error))
+    )
+  }
 }
