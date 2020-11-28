27abbd52a52603c295a2c13d1c2a98947a276661
Gabriel Geronimo <sr.gabrielgil@gmail.com>
2020-11-28 10:43:21 -0400

    inicio paciente detalle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 import { ListadoCitasComponent } from './listado-citas/listado-citas.component';
 import { ListadoPacienteComponent } from './listado-paciente/listado-paciente.component';
 import { LoginComponent } from './login/login.component';
+import { NuevaCitasComponent } from './nueva-citas/nueva-citas.component';
 import { NuevoPacienteComponent } from './nuevo-paciente/nuevo-paciente.component';
 import { PacienteDetalleComponent } from './paciente-detalle/paciente-detalle.component';
 import { PasswordConfigComponent } from './password-config/password-config.component';
@@ -20,7 +21,8 @@ const routes: Routes = [{path: '', component: LoginComponent},
                           {path : 'configuracion' , component : ConfiguracionComponent},
                           {path: 'password', component: PasswordConfigComponent},
                           {path: 'nuevo-paciente', component: NuevoPacienteComponent},
-                           {path: 'listado-citas', component: ListadoCitasComponent },
+                          {path: 'listado-citas', component: ListadoCitasComponent },
+                          {path: 'nueva-cita', component: NuevaCitasComponent },
                           {path: 'paciente-detalle/:id', component: PacienteDetalleComponent}]}
                          ];
 
